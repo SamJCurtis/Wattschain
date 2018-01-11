e05a727ecbcd8bcc5b552c1ad508dc68cfd51196
Sean <seansnider4@gmail.com>
2018-01-11 14:57:51 -0500

    skeleton

---
+++ wattscoin.py
@@ -0,0 +1,18 @@
+class Blcokchain(object):
+	def __init__(self):
+		self.chain = []
+		self.current_transactions = []
+
+	def new_block(self):
+		pass
+
+	def new_transactions(self):
+		pass
+
+	@staticmethod
+	def hash(block):
+		pass
+
+	@property
+	def last_block(self):
+		pass
